caadc67ffd477e87d7c6e0548d16f3f61de520d0
Juhye0k <kyoung1678@naver.com>
2025-07-19 19:47:40 +0900

    feat : 초기 개발환경 세팅

---
+++ src/main/java/law/counsel/CounselApplication.java
@@ -0,0 +1,13 @@
+package law.counsel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounselApplication.class, args);
+	}
+
+}
