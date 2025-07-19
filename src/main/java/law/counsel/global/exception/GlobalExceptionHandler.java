2f1a9b60a3aafed155896e8fcf49cbe43095f076
Juhye0k <kyoung1678@naver.com>
2025-07-20 00:09:51 +0900

    feat : 필터 추가

--- src/main/java/law/counsel/global/exception/GlobalExceptionHandler.java
+++ src/main/java/law/counsel/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package law.counsel.global.exception;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import law.counsel.global.response.ResponseBody;
 import law.counsel.global.response.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RequiredArgsConstructor
---
+++ src/main/java/law/counsel/global/jwt/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package law.counsel.global.jwt.handler;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import law.counsel.global.exception.ExceptionType;
+import law.counsel.global.response.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        objectMapper.writeValue(response.getWriter(), ResponseUtil.createFailureResponse(ExceptionType.ACCESS_DENIED));
+    }
+
+}
